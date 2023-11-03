ca36869ba77e866b928d7e3f06054fc028307512
Jay Turner <turnerj513@gmail.com>
2023-11-03 14:39:48 -0400

    added autoclicker and reset to JS

--- app.js
+++ app.js
@@ -1,9 +1,62 @@
-let donutCount = 0;
+class DonutMaker {
+    constructor() {
+        this.donutCount = 0;
+        this.autoClickerCount = 0;
+        this.autoClickerCost = 100;
+        this.autoClickerInterval = null;
+    }
 
-        const donutImage = document.getElementById('donutImage');
-        const donutCountElement = document.getElementById('donutCount');
+    clickDonut() {
+        this.donutCount++;
+    }
 
-        donutImage.addEventListener('click', () => {
-            donutCount++;
-            donutCountElement.textContent = donutCount;
-        });
+    buyAutoClicker() {
+        if (this.donutCount >= this.autoClickerCost) {
+            this.donutCount -= this.autoClickerCost;
+            this.autoClickerCount++;
+            this.autoClickerCost = Math.ceil(this.autoClickerCost * 1.1);
+        }
+    }
+
+    activateAutoClicker() {
+        if (this.autoClickerCount > 0 && !this.autoClickerInterval) {
+            this.autoClickerInterval = setInterval(() => {
+                this.clickDonut();
+                this.updateDisplay();
+            }, 1000);
+        }
+    }
+
+    resetGame() {
+        this.donutCount = 0;
+        this.autoClickerCount = 0;
+        this.autoClickerCost = 100;
+        clearInterval(this.autoClickerInterval);
+        this.autoClickerInterval = null;
+        this.updateDisplay();
+    }
+
+    updateDisplay() {
+        document.getElementById("donut-count").textContent = this.donutCount;
+        document.getElementById("auto-clicker-count").textContent = this.autoClickerCount;
+        document.getElementById("buy-auto-clicker").disabled = this.donutCount < this.autoClickerCost;
+    }
+}
+
+const donutMaker = new DonutMaker();
+
+document.getElementById("donut-click").addEventListener("click", () => {
+    donutMaker.clickDonut();
+    donutMaker.updateDisplay();
+});
+
+document.getElementById("buy-auto-clicker").addEventListener("click", () => {
+    donutMaker.buyAutoClicker();
+    donutMaker.activateAutoClicker();
+    donutMaker.updateDisplay();
+});
+
+document.getElementById("reset-game").addEventListener("click", () => {
+    donutMaker.resetGame();
+    donutMaker.updateDisplay();
+});
